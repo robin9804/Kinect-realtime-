44f556500146a4187ef8a4752a4cc8e8a20d5b91
YoungRok <51745277+robin9804@users.noreply.github.com>
2020-05-25 11:14:40 +0900

    main window xaml
    main window cs

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,354 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace WpfApp1
+{
+    using System;
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.Globalization;
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using Microsoft.Kinect;
+
+    /// <summary>
+    /// Interaction logic for MainWindow
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private const int MapDepthToByte = 8000 / 256; // 뎁스 레인지 설정.
+        private KinectSensor kinectSensor = null; // 키넥트 키는 부분
+
+        private DepthFrameReader depthFrameReader = null; // 뎁스 프레임 리더
+        private FrameDescription depthFrameDescription = null; // 뎁스에대한 정보 읽어오는 부분
+        private WriteableBitmap depthBitmap = null; // 비트맵으로 뎁스 바꿔주기
+        private byte[] depthPixels = null; // 뎁스맵 저장소
+
+        private ColorFrameReader colorFrameReader = null; // 컬러 프레임 가져오기
+        private WriteableBitmap colorBitmap = null; // 컬러 비트맵 읽어오기
+        private WriteableBitmap cropBitmap; 
+
+        static Int32Rect cropsize = new Int32Rect(305, 0, 1310, 1080);
+
+        private string statusText_color = null; // 스테이터스 color
+        private string statusText_depth = null; // 스테이터스 depth
+
+        // gamma 선언부
+        static double Gamma = 2.2; // 감마 값을 현재 1.0으로 해놨다.
+        static byte[] LUT(double gamma)
+        {
+            byte[] lut = new byte[256];
+            for (int i = 0; i < lut.Length; i++)
+            {
+                lut[i] = (byte)(Math.Pow(i / 255.0, 1.0 / gamma) * 255.0);
+            }
+            return lut;
+        }
+        byte[] lookup = LUT(Gamma);
+
+        public MainWindow() // 인스턴스 초기화
+        {
+
+            // get the kinectSensor object
+            this.kinectSensor = KinectSensor.GetDefault();
+
+            // open the reader for the depth frames
+            this.depthFrameReader = this.kinectSensor.DepthFrameSource.OpenReader();
+
+            // wire handler for frame arrival
+            this.depthFrameReader.FrameArrived += this.Reader_FrameArrived; // 뎁스 프레임 가져오기
+
+            this.colorFrameReader = this.kinectSensor.ColorFrameSource.OpenReader();// 컬러 프레임 가져오기
+            this.colorFrameReader.FrameArrived += this.Reader_ColorFrameArrived;
+            FrameDescription colorFrameDescription = this.kinectSensor.ColorFrameSource.CreateFrameDescription(ColorImageFormat.Bgra);
+            this.colorBitmap = new WriteableBitmap(colorFrameDescription.Width, colorFrameDescription.Height, 96.0, 96.0, PixelFormats.Bgr32, null); // 컬러 비트맵 쓰기
+            // 기존 이미지를 받아서 크롭
+            this.cropBitmap = new WriteableBitmap(1310,1080, 96.0, 96.0, PixelFormats.Bgr32, null);
+            //this.cropBitmap.Source = this.cropBitmap;
+            //this.cloneBitmap = new WriteableBitmap(this.cropBitmap);
+            
+            
+            // get FrameDescription from DepthFrameSource
+            this.depthFrameDescription = this.kinectSensor.DepthFrameSource.FrameDescription;
+            // allocate space to put the pixels being received and converted
+            this.depthPixels = new byte[this.depthFrameDescription.Width * this.depthFrameDescription.Height];
+            // create the bitmap to display
+            this.depthBitmap = new WriteableBitmap(this.depthFrameDescription.Width, this.depthFrameDescription.Height, 96.0, 96.0, PixelFormats.Gray8, null);
+            
+            
+            // set IsAvailableChanged event notifier 이벤트 가능한 부분을 열어두기
+            this.kinectSensor.IsAvailableChanged += this.Sensor_IsAvailableChanged;
+            // open the sensor
+            this.kinectSensor.Open();
+            // set the status text
+            this.StatusText = this.kinectSensor.IsAvailable ? Properties.Resources.RunningStatusText
+                                                            : Properties.Resources.NoSensorStatusText;
+            // use the window object as the view model in this simple example
+            this.DataContext = this;
+            // initialize the components (controls) of the window
+            this.InitializeComponent();
+        }
+
+        private void ColorFrameReader_FrameArrived(object sender, ColorFrameArrivedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// INotifyPropertyChangedPropertyChanged event to allow window controls to bind to changeable data
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Gets the bitmap to display
+        /// </summary>
+        public ImageSource DepthImageSource // 이미지 소스 게터를 사용함
+        {
+            get
+            {
+                return this.depthBitmap;
+            }
+        }
+        public ImageSource ColorImageSource
+        {
+            get
+            {
+                return this.cropBitmap;
+            }
+        }
+        /// <summary>
+        /// Gets or sets the current status text to display
+        /// </summary>
+        public string StatusText // 현재 상태 나타내기
+        {
+            get
+            {
+                return this.statusText_color;
+            }
+
+            set
+            {
+                if (this.statusText_color != value)
+                {
+                    this.statusText_color = value;
+
+                    // notify any bound elements that the text has changed
+                    if (this.PropertyChanged != null)
+                    {
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("StatusText"));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Execute shutdown tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void MainWindow_Closing(object sender, CancelEventArgs e) // 메인 윈도우 닫는 메소드
+        {
+            if (this.depthFrameReader != null)
+            {
+                // DepthFrameReader is IDisposable
+                this.depthFrameReader.Dispose();
+                this.depthFrameReader = null;
+            }
+
+            if (this.kinectSensor != null)
+            {
+                this.kinectSensor.Close();
+                this.kinectSensor = null;
+            }
+        }
+
+        /// <summary>
+        /// Handles the user clicking on the screenshot button
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void ScreenshotButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.depthBitmap != null)
+            {
+                // create a png bitmap encoder which knows how to save a .png file
+                BitmapEncoder encoder = new PngBitmapEncoder();
+
+                // create frame from the writable bitmap and add to encoder
+                encoder.Frames.Add(BitmapFrame.Create(this.depthBitmap));
+
+                string time = System.DateTime.UtcNow.ToString("hh'-'mm'-'ss", CultureInfo.CurrentUICulture.DateTimeFormat);
+
+                string myPhotos = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+                string path = Path.Combine(myPhotos, "KinectScreenshot-Depth-" + time + ".png");
+
+                // write the new file to disk
+                try
+                {
+                    // FileStream is IDisposable
+                    using (FileStream fs = new FileStream(path, FileMode.Create))
+                    {
+                        encoder.Save(fs);
+                    }
+
+                    this.StatusText = string.Format(CultureInfo.CurrentCulture, Properties.Resources.SavedScreenshotStatusTextFormat, path);
+                }
+                catch (IOException)
+                {
+                    this.StatusText = string.Format(CultureInfo.CurrentCulture, Properties.Resources.FailedScreenshotStatusTextFormat, path);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles the depth frame data arriving from the sensor
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        /// 센서로부터 영상 심도 프레임을 전달받는 부분
+        private void Reader_FrameArrived(object sender, DepthFrameArrivedEventArgs e) // 뎁스정보 가져오는 함수
+        {
+            bool depthFrameProcessed = false;
+
+            using (DepthFrame depthFrame = e.FrameReference.AcquireFrame())
+            {
+                if (depthFrame != null)
+                {
+                    // the fastest way to process the body index data is to directly access 
+                    // the underlying buffer
+                    using (Microsoft.Kinect.KinectBuffer depthBuffer = depthFrame.LockImageBuffer())
+                    {
+                        // verify data and write the color data to the display bitmap
+                        if (((this.depthFrameDescription.Width * this.depthFrameDescription.Height) == (depthBuffer.Size / this.depthFrameDescription.BytesPerPixel)) &&
+                            (this.depthFrameDescription.Width == this.depthBitmap.PixelWidth) && (this.depthFrameDescription.Height == this.depthBitmap.PixelHeight))
+                        {
+                            // Note: In order to see the full range of depth (including the less reliable far field depth)
+                            // we are setting maxDepth to the extreme potential depth threshold
+                            ushort maxDepth = ushort.MaxValue;
+
+                            // If you wish to filter by reliable depth distance, uncomment the following line:
+                            maxDepth = depthFrame.DepthMaxReliableDistance;
+
+                            this.ProcessDepthFrameData(depthBuffer.UnderlyingBuffer, depthBuffer.Size, depthFrame.DepthMinReliableDistance, maxDepth);
+                            depthFrameProcessed = true;
+                        }
+                    }
+                }
+            }
+
+            if (depthFrameProcessed)
+            {
+                this.RenderDepthPixels();
+            }
+        }
+
+        private static void CopyPixelsTo(BitmapSource sourceImage, Int32Rect sourceRoi, WriteableBitmap destinationImage, Int32Rect destinationRoi)
+        {
+            var croppedBitmap = new CroppedBitmap(sourceImage, sourceRoi);
+            int stride = croppedBitmap.PixelWidth * (croppedBitmap.Format.BitsPerPixel / 8);
+            var data = new byte[stride * croppedBitmap.PixelHeight];
+            croppedBitmap.CopyPixels(data, stride, 0);
+            destinationImage.WritePixels(destinationRoi, data, stride, 0);
+        }
+
+        private void Reader_ColorFrameArrived(object sender, ColorFrameArrivedEventArgs e) // 색상정보 가져오는 함수
+        {
+            // ColorFrame is IDisposable
+            using (ColorFrame colorFrame = e.FrameReference.AcquireFrame())
+            {
+                if (colorFrame != null)
+                {
+                    FrameDescription colorFrameDescription = colorFrame.FrameDescription;
+
+                    using (KinectBuffer colorBuffer = colorFrame.LockRawImageBuffer())
+                    {
+                        this.colorBitmap.Lock();
+
+                        // verify data and write the new color frame data to the display bitmap
+                        if ((colorFrameDescription.Width == 1920) && (colorFrameDescription.Height == 1080))
+                        //((colorFrameDescription.Width == this.colorBitmap.PixelWidth) && (colorFrameDescription.Height == this.colorBitmap.PixelHeight))
+                        {
+                            colorFrame.CopyConvertedFrameDataToIntPtr(
+                                this.colorBitmap.BackBuffer,
+                                (uint)(colorFrameDescription.Width * colorFrameDescription.Height * 4),
+                                //(uint)(1310*1080*4),
+                                ColorImageFormat.Bgra);
+
+                            this.colorBitmap.AddDirtyRect(new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight));
+                            
+                        }
+
+
+                        this.colorBitmap.Unlock();
+
+                        CopyPixelsTo(this.colorBitmap, cropsize, this.cropBitmap, new Int32Rect(0, 0, 1310, 1080));
+                    }
+                }
+            }
+        }
+
+        
+
+
+
+        /// <summary>
+        /// Directly accesses the underlying image buffer of the DepthFrame to 
+        /// create a displayable bitmap.
+        /// This function requires the /unsafe compiler option as we make use of direct
+        /// access to the native memory pointed to by the depthFrameData pointer.
+        /// </summary>
+        /// <param name="depthFrameData">Pointer to the DepthFrame image data</param>
+        /// <param name="depthFrameDataSize">Size of the DepthFrame image data</param>
+        /// <param name="minDepth">The minimum reliable depth value for the frame</param>
+        /// <param name="maxDepth">The maximum reliable depth value for the frame</param>
+        private unsafe void ProcessDepthFrameData(IntPtr depthFrameData, uint depthFrameDataSize, ushort minDepth, ushort maxDepth)
+        {
+            // depth frame data is a 16 bit value
+            ushort* frameData = (ushort*)depthFrameData;
+
+            // convert depth to a visual representation
+            for (int i = 0; i < (int)(depthFrameDataSize / this.depthFrameDescription.BytesPerPixel); ++i)
+            {
+                // Get the depth for this pixel
+                ushort depth = frameData[i];
+
+                // To convert to a byte, we're mapping the depth value to the byte range.
+                // Values outside the reliable depth range are mapped to 0 (black).
+                this.depthPixels[i] = (byte)(depth >= minDepth && depth <= maxDepth ? (depth / MapDepthToByte) : 0);
+                this.depthPixels[i] = lookup[this.depthPixels[i]];
+            }
+        }
+
+        /// <summary>
+        /// Renders color pixels into the writeableBitmap.
+        /// </summary>
+        private void RenderDepthPixels()
+        {
+            this.depthBitmap.WritePixels(
+                new Int32Rect(0, 0, this.depthBitmap.PixelWidth, this.depthBitmap.PixelHeight),
+                this.depthPixels,
+                this.depthBitmap.PixelWidth,
+                0);
+        }
+
+
+        /// <summary>
+        /// Handles the event which the sensor becomes unavailable (E.g. paused, closed, unplugged).
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        /// 센서 작동 확인부분
+        private void Sensor_IsAvailableChanged(object sender, IsAvailableChangedEventArgs e)
+        {
+            // on failure, set the status text
+            this.StatusText = this.kinectSensor.IsAvailable ? Properties.Resources.RunningStatusText
+                                                            : Properties.Resources.SensorNotAvailableStatusText;
+        }
+    }
+}
